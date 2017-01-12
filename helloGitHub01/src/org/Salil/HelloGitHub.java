74bea4427438fda4360a2094019d7035a0e2c3e3
Salil Chaubal <salil_chaubal@infosys.com>
2017-01-12 14:23:34 +0530

    adding helloGitHub.java file

---
+++ helloGitHub01/src/org/Salil/HelloGitHub.java
@@ -0,0 +1,14 @@
+package org.Salil;
+
+public class HelloGitHub {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello GitHub!!!");
+		
+	}
+
+}
